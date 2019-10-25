6579cc8ec50b5e78aa1ef613c426d0609fb00645
alton15 <alton15@naver.com>
2019-10-25 15:10:16 +0900

    Use Youtube Api
    listener로 영상 틀기, intent로 url 연결

--- UseYoutubeApi_Tutorial/app/src/main/java/com/example/useyoutubeapi_tutorial/MainActivity.java
+++ UseYoutubeApi_Tutorial/app/src/main/java/com/example/useyoutubeapi_tutorial/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.useyoutubeapi_tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    YouTubePlayerView view_youtube;
+    Button btn_youtube, btn_subscribe;
+    YouTubePlayer.OnInitializedListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_youtube = findViewById(R.id.btn_youtube);
+        btn_subscribe = findViewById(R.id.btn_subscribe);
+        view_youtube = findViewById(R.id.view_youtube);
+
+        listener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("Hkg6Vu2Um5k");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        btn_youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view_youtube.initialize("AIzaSyC_5QAUx-kl5y8tVIG9xxgC-mehkQG8rmA", listener);
+            }
+        });
+
+        btn_subscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.youtube.com/channel/UCBkyj16n2snkRg1BAzpovXQ";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }

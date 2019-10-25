dce878b90f206ff883cfa61abb4b0aa8cea090d5
alton15 <alton15@naver.com>
2019-10-25 18:41:01 +0900

    [Update] 버튼을 누르면 도시로 이동

--- UseGoogleMap_Tutorial/app/src/main/java/com/example/usegooglemap_tutorial/MainActivity.java
+++ UseGoogleMap_Tutorial/app/src/main/java/com/example/usegooglemap_tutorial/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.usegooglemap_tutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +16,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button btn_seoul, btn_daejeon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +26,49 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(MainActivity.this);
+
+        btn_seoul = findViewById(R.id.btn_seoul);
+        btn_daejeon = findViewById(R.id.btn_daejeon);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
 
-        mMap = googleMap;
+        btn_seoul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap = googleMap;
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+                LatLng SEOUL = new LatLng(37.56, 126.97);
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(SEOUL);
+                markerOptions.title("서울");
+                markerOptions.snippet("한국의 수도");
+                mMap.addMarker(markerOptions);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+            }
+        });
+
+        btn_daejeon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMap = googleMap;
+
+                LatLng DAEJEON = new LatLng(36.35, 127.38);
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(DAEJEON);
+                markerOptions.title("대전");
+                markerOptions.snippet("한국의 중심");
+                mMap.addMarker(markerOptions);
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(DAEJEON));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+            }
+        });
 
     }
 }

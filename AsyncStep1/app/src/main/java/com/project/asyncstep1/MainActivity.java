215fb20414e50c332df96ad89b7954f1fb7fa382
alton15 <alton15@naver.com>
2019-11-26 20:15:08 +0900

    Create AsyncTask

---
+++ AsyncStep1/app/src/main/java/com/project/asyncstep1/MainActivity.java
@@ -0,0 +1,60 @@
+package com.project.asyncstep1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btn_toast1, btn_toast2, btn_sleep, btn_task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_toast1 = findViewById(R.id.btn_toast1);
+        btn_toast1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayToast(btn_toast1);
+            }
+        });
+        btn_toast2 = findViewById(R.id.btn_toast2);
+        btn_toast2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayToast(btn_toast2);
+            }
+        });
+
+        btn_sleep = findViewById(R.id.btn_sleep);
+        btn_sleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sleepUntilFor(btn_sleep);
+            }
+        });
+
+        btn_task = findViewById(R.id.btn_task);
+    }
+
+    public void displayToast(View v) {
+        Button btn = (Button)v;
+        String strMsg = btn.getText().toString();
+        Toast.makeText(this, strMsg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void sleepUntilFor(View v) {
+        int s = 10000;
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

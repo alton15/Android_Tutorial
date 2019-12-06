7dc09d320087709b721ed9199170e7ce385c3066
alton15 <alton15@naver.com>
2019-12-06 16:24:38 +0900

    Update MainActivity.java

--- BottomSheet_tutorial/app/src/main/java/com/project/bottomsheet_tutorial/MainActivity.java
+++ BottomSheet_tutorial/app/src/main/java/com/project/bottomsheet_tutorial/MainActivity.java
@@ -1,14 +1,36 @@
 package com.project.bottomsheet_tutorial;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout llBottomSheet;
+
+    FloatingActionButton fabButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        llBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+        fabButton = (FloatingActionButton) findViewById(R.id.fab);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "FAB clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
